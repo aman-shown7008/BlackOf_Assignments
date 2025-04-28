16eee0a6818074e9e4a8c802134eac1aaf1ad499
Aman Mishra <74037648+aman-shown7008@users.noreply.github.com>
2025-04-28 07:30:19 +0530

    Home.tsx

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -4,21 +4,21 @@ import bgVideo from "../../assets/new_video.mp4";
 
 const Home: React.FC = () => {
   return (
-    <Box sx={{ position: "relative", width: "100%", height: "100vh", overflow: "hidden", }}>
+    <Box sx={{ position: "relative", width: "100%", minHeight: {xs: "80vh", md: "100vh"}, overflow: "hidden", }}>
       <video autoPlay loop muted playsInline style={{ position: "absolute", width: "100%", height: "100%", objectFit: "cover", zIndex: -1, }}>
         <source src={bgVideo} type="video/mp4" />
       </video>
 
       <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", textAlign: "center", color: "#fff", width: { xs: "90%", sm: "80%", md: "60%" },}}>
-        <Typography variant="body1" sx={{ fontSize: { xs: "18px", sm: "20px", md: "25px" }, fontFamily: '"Poppins", sans-serif', }}>
+        <Typography variant="body1" sx={{ fontSize: { sm: "22px", md: "25px" }, fontFamily: '"Poppins", sans-serif', }}>
           Driven by performance
         </Typography>
 
-        <Typography variant="h4" sx={{ fontWeight: "bold", fontSize: { xs: "24px", sm: "32px", md: "55px" }, fontFamily: '"Poppins", sans-serif', py: 2, }} >
+        <Typography variant="h4" sx={{ fontWeight: "bold", fontSize: { sm: "35px", md: "55px" }, fontFamily: '"Poppins", sans-serif', py: 2, }} >
           Soft Trims and{" "} <span style={{ color: "#00BFFF" }}>NVH Solutions</span>
         </Typography>
 
-        <Typography variant="h5" sx={{ fontSize: { xs: "18px", sm: "20px", md: "25px" }, fontFamily: '"Poppins", sans-serif', }}>
+        <Typography variant="h5" sx={{ fontSize: {sm: "20px", md: "25px" }, fontFamily: '"Poppins", sans-serif', }}>
           for seamless rides
         </Typography>
       </Box>
